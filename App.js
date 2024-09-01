0eedd0a450ac099223b3e3fa20ec28605195b355
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-09-01 10:45:14 +0300

    Lisäsin laskimeen historian sekä tarkistuksen että on numero

--- App.js
+++ App.js
@@ -1,62 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 import { useState } from 'react';
 
-export default function App() {
-  const [numero, setNumero] = useState("");
-  const [toinenNumero, setToinenNumero] = useState("");
-  const [tulos, setTulos] = useState("");
 
+
+export default function App() {
+  const [numero, setNumero] = useState(0);
+  const [toinenNumero, setToinenNumero] = useState(0);
+  const [tulos, setTulos] = useState(0);
+  const [historys, setHistorys] = useState([]);
+  const [error, setError] = useState("");
 
   const buttonPressed = (merkki) => {
     const num1 = parseFloat(numero);
     const num2 = parseFloat(toinenNumero);
 
+    if (isNaN(num1) || isNaN(num2)) {
+      setError("Vain numeroita kiitos")
+      return
+    }
+    setError("")
+    let calculation;
     if (merkki === "+") {
       setTulos((num1 + num2).toString());
-      setNumero("")
-      setToinenNumero("")
+      calculation = num1 + " " + merkki + " " + num2 + " = " + (num1 + num2);
     }
     else if (merkki === "-") {
       setTulos((num1 - num2).toString());
-      setNumero("")
-      setToinenNumero("")
+      calculation = num1 + " " + merkki + " " + num2 + " = " + (num1 - num2);
     }
+    setNumero("")
+    setToinenNumero("")
+
+    setHistorys([...historys, calculation]);
   }
 
   return (
     <View style={styles.container}>
+      <Text>{error}</Text>
       <Text>Result: {tulos}</Text>
+
       <TextInput
-        style={{
-          height: 40,
-          width: 200,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
+        style={styles.inputs}
         placeholder='Numero tähän'
         onChangeText={numero => setNumero(numero)}
         value={numero}
         keyboardType="numeric"
       />
       <TextInput
-        style={{
-          height: 40,
-          width: 200,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
+        style={styles.inputs}
         placeholder='Toinen numero tähän'
         onChangeText={toinenNumero => setToinenNumero(toinenNumero)}
         value={toinenNumero}
         keyboardType="numeric"
       />
-      <View style={{
-
-        flexDirection: "row",
-        paddingVertical: 10,
-        gap: 20
-      }}>
+      <View style={styles.button}>
         <Button
 
           onPress={() => buttonPressed("+")} title="+" />
@@ -65,6 +63,14 @@ export default function App() {
           onPress={() => buttonPressed("-")} title="-" />
 
       </View>
+      <Text>History</Text>
+      <FlatList
+        keyExtractor={(item, index) => index}
+        data={historys}
+        renderItem={({ item }) => <Text>{item}</Text>}
+        ListEmptyComponent={<Text>No calculation</Text>}
+        inverted
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -72,11 +78,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50
   },
+  button: {
+    flexDirection: "row",
+    paddingVertical: 10,
+    gap: 20
+  },
+  inputs: {
+    height: 40,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+
 
 });
 // Otin mallia https://www.geeksforgeeks.org/build-a-calculator-using-react-native/
